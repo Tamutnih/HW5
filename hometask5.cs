6299d36cfb710d26452ea2a80eca4ebbd96d3fa0
tatyana mutnykh <renu07@list.ru>
2022-08-19 23:44:11 +0500

    решение

--- hometask5.cs
+++ hometask5.cs
@@ -1,39 +1,111 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
+// Console.Clear();
+// Console.WriteLine("Введите размер массива");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] numbers = new int[size];
+// FillArrayRandomNumbers(numbers);
+// Console.WriteLine("Массив: ");
+// PrintArray(numbers);
+// int count = 0;
+
+// for (int j = 0; j < numbers.Length; j++)
+// if (numbers[j] % 2 == 0)
+// count++;
+
+// Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
+
+// void FillArrayRandomNumbers(int[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+//     {
+//         numbers[i] = new Random().Next(100,1000);
+//     }
+// }
+// void PrintArray(int[] numbers)
+// {
+//     Console.Write("[ ");
+//     for(int i = 0; i < numbers.Length; i++)
+//     {
+//         Console.Write(numbers[i] + ", ");
+//     }
+//     Console.Write("]");
+//     Console.WriteLine();
+// }
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
 Console.Clear();
-int[] numbers = new int[10];
+Console.Write($"Введите количество элементов массива: ");
+int numberElements = Convert.ToInt32(Console.ReadLine()); 
 
-void FillArray(int[] array, int min, int max){
-  for (int i = 0; i<array.Length; i++ )
+int RandomNumbers(int numberElements, int min, int max)
   {
-    array[i] = new Random().Next(min, max);
-  }
-}
+  int[] randomNumbers = new int[numberElements];
+  int sumElements = 0;
+  Console.Write("Массив: ");
 
-void WriteArray(int[] array){
-    for (int i = 0; i<array.Length; i++)
+    for (int i = 0; i <randomNumbers.Length; i+=2)
     {
-    Console.Write(array[i] + " ");
-  }
-  Console.WriteLine();
-}
+      randomNumbers[i] = new Random().Next(min, max);
 
-int QuantityPositive(int[] array)
-{
-    int quantity = 0;
-    for (int i = 0; i<array.Length; i++)
-    {
-    if (array[i] % 2 == 1)
-    {
-      quantity++;
+      Console.Write(randomNumbers[i] + ", ");
+
+      if (i % 2 != 1)
+      {
+        sumElements = sumElements + randomNumbers[i];
+      }
     }
+  return sumElements;
   }
-  return quantity;
-}
 
-FillArray(numbers, 100, 1000);
-WriteArray(numbers);
-Console.WriteLine();
+int randomNumbers =  RandomNumbers(numberElements, 1, 10);
 
-int quantity = QuantityPositive(numbers);
-Console.WriteLine($"Количество чётных чисел в массиве: {quantity}");
+Console.WriteLine($"\n Сумма элементов, стоящих на нечётных позициях: {randomNumbers}");
+
+
+//  Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// Console.Clear();
+// Console.WriteLine("Введите размер массива: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// double[] numbers = new double[size];
+// FillArrayRandomNumbers(numbers);
+// Console.WriteLine("Массив: ");
+// PrintArray(numbers);
+// double min = Int32.MaxValue;
+// double max = Int32.MinValue;
+
+// for (int j = 0; j < numbers.Length; j++)
+// {
+//     if (numbers[j] > max)
+//         {
+//             max = numbers[j];
+//         }
+//     if (numbers[j] < min)
+//         {
+//             min = numbers[j];
+//         }
+// }
+
+// Console.WriteLine($"всего {numbers.Length} чисел. Максимальное значение = {max}, минимальное значение = {min}");
+// Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+
+// void FillArrayRandomNumbers(double[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+//         {
+//             numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+//         }
+// }
+// void PrintArray(double[] numbers)
+// {
+//     Console.Write("[ ");
+//     for(int i = 0; i < numbers.Length; i++)
+//         {
+//             Console.Write(numbers[i] + " ");
+//         }
+//     Console.Write("]");
+//     Console.WriteLine();
+// }
+ 
