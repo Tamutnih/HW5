8ca6f90e17cd531b87dc97181ddb663bb2e8c9a0
Tamutnih <109300178+Tamutnih@users.noreply.github.com>
2022-08-21 21:09:27 +0500

    Update hometask5.cs

--- hometask5.cs
+++ hometask5.cs
@@ -109,3 +109,46 @@ Console.WriteLine($"\n Сумма элементов, стоящих на неч
 //     Console.WriteLine();
 // }
  
+//Вариант 2 - целочисленные числа
+//  Console.Clear();
+// Console.WriteLine("Введите размер массива: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] numbers = new int[size];
+// FillArrayRandomNumbers(numbers);
+// Console.WriteLine("Массив: ");
+// PrintArray(numbers);
+// int min = Int32.MaxValue;
+// int max = Int32.MinValue;
+
+// for (int j = 0; j < numbers.Length; j++)
+// {
+//     if (numbers[j] > max)
+//         {
+//             max = numbers[j];
+//         }
+//     if (numbers[j] < min)
+//         {
+//             min = numbers[j];
+//         }
+// }
+
+// Console.WriteLine($"всего {numbers.Length} чисел. Максимальное значение = {max}, минимальное значение = {min}");
+// Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+
+// void FillArrayRandomNumbers(int[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+//         {
+//             numbers[i] = Convert.ToInt32(new Random().Next(100,1000)) / 100;
+//         }
+// }
+// void PrintArray(int[] numbers)
+// {
+//     Console.Write("[ ");
+//     for(int i = 0; i < numbers.Length; i++)
+//         {
+//             Console.Write(numbers[i] + ", ");
+//         }
+//     Console.Write("]");
+//     Console.WriteLine();
+//
